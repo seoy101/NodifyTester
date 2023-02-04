285a24c0054e05e4048eaaefad743a90f2c4d0ed
Young Jun Seo <fighton.yj@gmail.com>
2023-02-04 16:17:38 +0900

    초기 커밋.

---
+++ AppViewModel.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+namespace NodifyTester
+{
+    public class ConnectionViewModel
+    {
+        public ConnectorViewModel? Source { get; set; }
+        public ConnectorViewModel? Target { get; set; }
+    }
+
+    public class ConnectorViewModel : INotifyPropertyChanged
+    {
+        private Point _anchor;
+        public Point Anchor
+        {
+            set
+            {
+                _anchor = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Anchor)));
+            }
+            get => _anchor;
+        }
+
+        public string? Title { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class NodeViewModel
+    {
+        public string? Title { get; set; }
+
+        public ObservableCollection<ConnectorViewModel> Input { get; set; } = new ObservableCollection<ConnectorViewModel>();
+        public ObservableCollection<ConnectorViewModel> Output { get; set; } = new ObservableCollection<ConnectorViewModel>();
+    }
+
+    public class EditorViewModel
+    {
+        public ObservableCollection<NodeViewModel> Nodes { get; } = new ObservableCollection<NodeViewModel>();
+        public ObservableCollection<ConnectionViewModel> Connections { get; } = new ObservableCollection<ConnectionViewModel>();
+
+
+        public EditorViewModel()
+        {
+            // 하나 추가해 줌.
+            Nodes.Add(new NodeViewModel 
+            {
+                Title = "Welcome",
+                Input = new ObservableCollection<ConnectorViewModel>
+                {
+                    new ConnectorViewModel
+                    {
+                        Title = "In"
+                    }
+                },
+                Output = new ObservableCollection<ConnectorViewModel>
+                {
+                    new ConnectorViewModel
+                    {
+                        Title = "Out"
+                    }
+                }
+            });
+
+            // 두개 추가해 줌.
+            var welcome = new NodeViewModel
+            {
+                Title = "Welcome",
+                Input = new ObservableCollection<ConnectorViewModel>
+            {
+                new ConnectorViewModel
+                {
+                    Title = "In"
+                }
+            },
+                Output = new ObservableCollection<ConnectorViewModel>
+            {
+                new ConnectorViewModel
+                {
+                    Title = "Out"
+                }
+            }
+            };
+
+            var nodify = new NodeViewModel
+            {
+                Title = "To Nodify",
+                Input = new ObservableCollection<ConnectorViewModel>
+            {
+                new ConnectorViewModel
+                {
+                    Title = "In"
+                }
+            }
+            };
+
+            Nodes.Add(welcome);
+            Nodes.Add(nodify);
+
+            Connections.Add(new ConnectionViewModel
+            {
+                Source = welcome.Output[0],
+                Target = nodify.Input[0]
+            });
+        }
+
+    }
+}
